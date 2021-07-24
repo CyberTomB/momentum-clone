290e3015d0a84a6526d6e14810a2a244876643a4
CyberDack <afntwrk.guyrpg@gmail.com>
2021-07-24 12:50:26 -0600

    quotes getter works

--- app/Services/BackgroundService.js
+++ app/Services/BackgroundService.js
@@ -7,10 +7,10 @@ class BackgroundService {
    async getBackground() {
       let res = await sandBoxApi.get('images')
       console.log(res.data)
-      document.body.style.backgroundImage = `url(${res.data.imgUrl})`
+      document.body.style.backgroundImage = `url(${res.data.largeImgUrl})`
       document.getElementById('image-desc').innerHTML =
          `<h6>Image by: ${res.data.author}</h6>
-      <a href="${res.data.url}">${res.data.url}</a>`
+      <a href="${res.data.url}" class="mdi mdi-link"> image</a>`
 
       // document.getElementById('img-author').innerText = res.data.author
       // document.getElementById('img-url').innerText = res.data.url
--- app/Services/QuotesService.js
+++ app/Services/QuotesService.js
@@ -4,6 +4,10 @@ class QuotesService {
    async getQuote() {
       let res = await sandBoxApi.get('quotes')
       console.log(res.data)
+      document.getElementById('quote').innerHTML = `
+      <h4>"${res.data.content}"</h4>
+                <h6 class="author">${res.data.author}</h6>
+      `
    }
 }
 
